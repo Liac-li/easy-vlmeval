0af4a5c93d73c3181d5911bb9a6b13837059c437
lyt <li_yantao@samil.nju.edu.cn>
2025-04-28 15:13:55 +0800

    [refactor] unify data structure for mmmu

--- src/datasets/mmmu.py
+++ src/datasets/mmmu.py
@@ -2,141 +2,256 @@
 """Utils for data load, save, and process (e.g., prompt construction)"""
 
 import os
+from typing import List, Dict, Any, Literal, Optional
+
+from datasets import load_dataset, concatenate_datasets
+
+mmmu_default_config = """
+task_instructions:
+- ""
+multi_choice_example_format:
+- "{}
+
+{}
+
+Answer with the option's letter from the given choices directly."
+
+short_ans_example_format:
+- "{}
+
+Answer the question using a single word or phrase."
+"""
+
+"""Utils for data load, save, and process (e.g., prompt construction)"""
+
+import os
 import json
 import yaml
-import ast
 import re
-from typing import List, Dict, Any
-from PIL import Image
+
+
+DOMAIN_CAT2SUB_CAT = {
+  'Art and Design': ['Art', 'Art_Theory', 'Design', 'Music'],
+  'Business': ['Accounting', 'Economics', 'Finance', 'Manage','Marketing'],
+  'Science': ['Biology', 'Chemistry', 'Geography', 'Math', 'Physics',],
+  'Health and Medicine': ['Basic_Medical_Science', 'Clinical_Medicine', 'Diagnostics_and_Laboratory_Medicine', 'Pharmacy', 'Public_Health'],
+  'Humanities and Social Science': ['History', 'Literature', 'Sociology', 'Psychology'],
+  'Tech and Engineering': ['Agriculture', 'Architecture_and_Engineering', 'Computer_Science', 'Electronics', 'Energy_and_Power', 'Materials', 'Mechanical_Engineering'],
+}
+
+
+CAT_SHORT2LONG = {
+    'acc': 'Accounting',
+    'agri': 'Agriculture',
+    'arch': 'Architecture_and_Engineering',
+    'art': 'Art',
+    'art_theory': 'Art_Theory',
+    'bas_med': 'Basic_Medical_Science',
+    'bio': 'Biology',
+    'chem': 'Chemistry',
+    'cli_med': 'Clinical_Medicine',
+    'cs': 'Computer_Science',
+    'design': 'Design',
+    'diag_med': 'Diagnostics_and_Laboratory_Medicine',
+    'econ': 'Economics',
+    'elec': 'Electronics',
+    'ep': 'Energy_and_Power',
+    'fin': 'Finance',
+    'geo': 'Geography',
+    'his': 'History',
+    'liter': 'Literature',
+    'manage': 'Manage',
+    'mark': 'Marketing',
+    'mate': 'Materials',
+    'math': 'Math',
+    'mech': 'Mechanical_Engineering',
+    'music': 'Music',
+    'phar': 'Pharmacy',
+    'phys': 'Physics',
+    'psy': 'Psychology',
+    'pub_health': 'Public_Health',
+    'socio': 'Sociology'
+}
+
+# DATA SAVING
+def save_json(filename, ds):
+    with open(filename, 'w') as f:
+        json.dump(ds, f, indent=4)
+
+
+def get_multi_choice_info(options):
+    """
+    Given the list of options for multiple choice question
+    Return the index2ans and all_choices
+    """
+    
+    start_chr = 'A'
+    all_choices = []
+    index2ans = {}
+    for i, option in enumerate(options):
+        index2ans[chr(ord(start_chr) + i)] = option
+        all_choices.append(chr(ord(start_chr) + i))
+
+    return index2ans, all_choices
+
+def load_yaml(file_path):
+    with open(file_path, 'r') as stream:
+        try:
+            yaml_dict = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    return yaml_dict
+
+
+def parse_img_path(text):
+    matches = re.findall("<img='(.*?)'>", text)
+    return matches
+
+def process_single_sample(data):
+    question = data['question']
+    o_imgs_paths = []
+    for option in data['options']:
+        current_o_imgs_paths = parse_img_path(option)
+        for img_path in current_o_imgs_paths:
+            o_imgs_paths.append(img_path)
+
+    if len(o_imgs_paths) > 1:  # multiple images in options, used for random selection
+        return {'id': data['id'], 'question': question, 'options': data['options'], 'answer': data['answer'],
+             'image': None, 'question_type': data['question_type']}
+    else:
+        return {'id': data['id'], 'question': question, 'options': data['options'], 'answer': data['answer'],
+             'image': data['image_1'], 'question_type': data['question_type']}
+
+
+# DATA SAVING
+def save_json(filename, ds):
+    with open(filename, 'w') as f:
+        json.dump(ds, f, indent=4)
+
+def save_jsonl(filename, data):
+    """
+    Save a dictionary of data to a JSON Lines file with the filename as key and caption as value.
+
+    Args:
+        filename (str): The path to the file where the data should be saved.
+        data (dict): The dictionary containing the data to save where key is the image path and value is the caption.
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        for img_path, caption in data.items():
+            # Extract the base filename without the extension
+            base_filename = os.path.basename(img_path)
+            # Create a JSON object with the filename as the key and caption as the value
+            json_record = json.dumps({base_filename: caption}, ensure_ascii=False)
+            # Write the JSON object to the file, one per line
+            f.write(json_record + '\n')
+
+def save_args(args, path_dir):
+    argsDict = args.__dict__
+    with open(path_dir + 'setting.txt', 'w') as f:
+        f.writelines('------------------ start ------------------' + '\n')
+        for eachArg, value in argsDict.items():
+            f.writelines(eachArg + ' : ' + str(value) + '\n')
+        f.writelines('------------------- end -------------------')
+
+
+
+# DATA PROCESSING
+def construct_prompt(sample, config):
+    question = sample['question']
+    options = eval(sample['options'])
+    example = ""
+    if sample['question_type'] == 'multiple-choice':
+        start_chr = 'A'
+        prediction_range = []
+        index2ans = {}
+        for option in options:
+            prediction_range.append(start_chr)
+            example += f"({start_chr}) {option}\n"
+            index2ans[start_chr] = option
+            start_chr = chr(ord(start_chr) + 1)
+        empty_prompt_sample_structure = config['multi_choice_example_format']
+        empty_prompt = empty_prompt_sample_structure.format(question, example)
+        res_dict = {}
+        res_dict['index2ans'] = index2ans
+        res_dict['correct_choice'] = sample['answer']
+        res_dict['all_choices'] = prediction_range
+        res_dict['empty_prompt'] = empty_prompt
+        if config['task_instructions']:
+            res_dict['final_input_prompt'] = config['task_instructions'].strip() + '\n\n' + empty_prompt
+        else:
+            res_dict['final_input_prompt'] = empty_prompt
+
+        res_dict['gt_content'] = options[ord(sample['answer'].upper()) - ord('A')]
+    else:
+        empty_prompt_sample_structure = config['short_ans_example_format']
+        empty_prompt = empty_prompt_sample_structure.format(question)
+        res_dict = {}
+        res_dict['empty_prompt'] = empty_prompt
+        if config['task_instructions']:
+            res_dict['final_input_prompt'] = config['task_instructions'].strip() + '\n\n' + empty_prompt
+        else:
+            res_dict['final_input_prompt'] = empty_prompt
+        res_dict['gt_content'] = sample['answer']
+
+    res_dict.update(sample)
+    return res_dict
+
+
 class MMMUDataset:
-    def __init__(self,data_fold: str , split_type:str , file_type = ".json"):
+
+    def __init__(self, 
+                 split: Literal["dev", "test", "validation"] = "dev",
+                 data_dir: str = "data/MMMU",
+                 reuse_saved: Optional[str] = None):
         """
         初始化 MMMUDataset 类。
         :param data_path: 数据文件(Parquet)的本地路径。
         """
-        self.dataname = "mmmu"
-        self.data_fold = data_fold
-        self.split_type = split_type
 
-        self.data_path = os.path.join(self.data_fold , self.split_type) + file_type
-
-
-        if self.data_path.endswith(".parquet"):
-            self.data = self._load_parquet()
-        elif self.data_path.endswith(".json"):
-            self.data = self._load_json()
-        else:
-            raise ValueError(f"不支持的文件格式: {self.data_path}，请提供 .parquet 或 .json 文件")
-
-    def _load_parquet(self) -> dict:
-        """
-        读取 Parquet 文件并返回 Dict。
-        """
-        import pandas as pd
-        try:
-            df = pd.read_parquet(self.data_path)
-            return df.to_dict(orient="records") 
-        except Exception as e:
-            print(f"读取 Parquet 文件时出错: {e}")
-            return []
-
-    def _load_json(self) -> dict:
-        """
-        读取 JSON 文件并返回 Dict。
-        """
-        try:
-            import json
-            with open(self.data_path, "r", encoding="utf-8") as f:
-                data = json.load(f)
-            return data
-        except Exception as e:
-            print(f"读取 JSON 文件时出错: {e}")
-            return []
-
-    def _build_prompt(self, item: Dict[str, Any], template: Dict[str, str]) -> str:
-        """
-        构建提示词
+        self.data_dir = data_dir
+        self.split = split
         
-        Args:
-            item: 数据项
-            template: 提示词模板
-            
-        Returns:
-            构建好的提示词 prompts
-        """
-        return self._replace_images_tokens(template["user"].format(**item))
+        self.config = yaml.safe_load(mmmu_default_config)
+        for key, value in self.config.items():
+            if key != 'eval_params' and type(value) == list:
+                assert len(value) == 1, 'key {} has more than one value'.format(key)
+                self.config[key] = value[0]
 
-    def _load_yaml(self,prompt_path):
-        """
-            下载不同task的prompt
-        """
-        import yaml
-        with open(prompt_path, "r", encoding="utf-8") as f:
-            data = yaml.safe_load(f)
-        return data["prompts"][self.dataname]
-        
-    def _parse_options(self, options:List[str]):
-        """
-        将options加上选项ABCD
-        """
+        self.data = self._load_dataset()
+        self.data = self.convert_to_inference_format()
 
-        option_letters = [chr(ord("A") + i) for i in range(len(options))]
-        choices_str = "\n".join([f"{option_letter}. {option}" for option_letter, option in zip(option_letters, options)])
-        return choices_str
+    def _load_dataset(self):
+        sub_dataset_list = []
+        for subject in CAT_SHORT2LONG.values():
+            sub_dataset = load_dataset(self.data_dir, subject, split=self.split)
+            sub_dataset_list.append(sub_dataset)
+        dataset = concatenate_datasets(sub_dataset_list)
+        return dataset
     
-    def _replace_images_tokens(self , input_string):
-        image_order = [int(num) for num in re.findall(r"<image\s+(\d+)>", input_string)]
-        input_string = re.sub(r"<image\s+\d+>", "[image]", input_string)
+    def convert_to_inference_format(self):
 
-        return input_string ,image_order
+        converted_data_list = []
+        for sample in self.data:
+            sample = process_single_sample(sample)
+            sample = construct_prompt(sample, self.config)
 
-    def build_input(self , batch: List[Dict], prompt_path):
-        """
-        构建一批输入，处理多图片情况
-        这里的batch是指模型下载
-        """
-        self.prompt_template = self._load_yaml(prompt_path)
-        inputs = []
-        for item in batch:
-            item["options"] = self._parse_options(ast.literal_eval(str(item["options"])))
+            converted_data = {
+                "id": sample['id'],
+                "query": sample['final_input_prompt'],
+                "answer": sample['answer'],
+                "image": {
+                    "path": None,
+                    "item": sample['image']
+                }
+            } 
+            sample.pop('image')
+            converted_data['origin_data'] = sample
+            converted_data_list.append(converted_data)
 
-
-            prompts , image_order = self._build_prompt(item, self.prompt_template)
-            images = []
-            # 根据MMMU数据集，最多有7张图片
-            for i in image_order:
-                image_key = f"image_{i}_path"
-                if image_key in item:
-                    img_path = os.path.join(self.data_fold, item[image_key])
-                    img = Image.open(img_path).convert('RGB')
-                    images.append(img)
-
-            if len(images) == 1:
-                multi_modal_data = {"image": images[0]}
-            else:
-                multi_modal_data = {"images": images}
-
-            inputs.append({
-                "prompt": prompts,
-                "multi_modal_data": multi_modal_data
-            })
-        return inputs
-
-    def save_indill(self, response):
-        """
-        30个类别得到结果后,将response传入来分开不同的类
-        """
-        
-        # if os.path.exists(self.json_path):
-        #     try:
-        #         with open(self.json_path, 'r', encoding='utf-8') as f:
-        #             config = json.load(f)
-        #     except Exception as e:
-        #         print(f"加载配置文件失败: {e}")
-        pass
-
+        return converted_data_list
+            
 
 if __name__ == "__main__":
-    mmmu = MMMUDataset("data/MMMU" , "dev")
-    print(mmmu.build_input(mmmu.data[0:10] , "src/config/prompts_config.yaml"))
+    mmmu = MMMUDataset()
 
